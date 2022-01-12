446d0ad3c7e9e7afef762ad3ada330b8fcc54fc8
Vitaliy <example.com>
2022-01-12 18:06:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// alert('Hello');
+
+// const qwetion = confirm('Are you hare?');
+// console.log(qwetion);
+
+// const answer = +prompt("How old are you?", "18");
+// console.log(answer + 5); 
+
+// const answers = [];
+
+// answers[0] = prompt('What your name?', '');
+// answers[1] = prompt('What your last name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(typeof(answers)); 
+
+
+//інтерполяція
+// const category = 'toys';
+// console.log(`https://someurl/${category}/5`);
+
+
+// const user = "Vitaliy";
+// alert(`Hello, ${user}`);
+
+
+//оператори в js + - * /
+
+//increment and decrement
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+    //postfix form record
+// console.log(incr++); //10
+// console.log(decr--); //10
+    //prefix 
+// console.log(++incr); //11
+// console.log(--decr); //9 
+
+
+// const isChecked = true,
+//       isClose = false;
+// console.log(isChecked && isClose); //false
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked || !isClose); //true тому що бодай одна умова, але true(правдива)
+
+// && - and 
+// || - or
+
+// ! - оператор заперечення true => false && false => true
+console.log(2 + 2 * 2 !== '6');//true
+
+
+
